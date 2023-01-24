4389848d99bed8104b694c30e3691d4ee4a90836
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-24 07:06:25 +0300

    Updating the UI of the weather app with values fetched from the API

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,28 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+//Function to update the UI
+const updateUI = (data) => {
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML =`
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+        `;
+
+    // remove the d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+};
+
 
 //is an async function becasue in this function, we will be calling
 //asynchronous function and thus it is going to take some time to complete
@@ -20,9 +44,7 @@ cityForm.addEventListener('submit', (e) => {
 
     //update the ui with new city
     updateCity(city)
-        .then(data => {
-            console.log(data)
-        })
+        .then(data => updateUI(data))
         .catch((e) => {
             console.log("ERROR!!", e)
         })
