a4ab99f7a5cd997bd49c5aefd5674f1e68381624
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-25 06:19:11 +0300

    Refactoring to use the ternary operator

--- scripts/app.js
+++ scripts/app.js
@@ -27,13 +27,16 @@ const updateUI = (data) => {
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute('src', iconSrc)
 
-    let timeSrc = null;
-    if(weather.IsDayTime){
-        timeSrc = 'img/day.svg'
-    } else{
-        timeSrc = 'img/night.svg'
-    }
-    time.setAttribute('src', timeSrc);
+    //Ternary operator
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    
+    //THE ABOVE LINES REPLACES THIS CODE BELOW
+    // if(weather.IsDayTime){
+    //     timeSrc = 'img/day.svg'
+    // } else{
+    //     timeSrc = 'img/night.svg'
+    // }
+     time.setAttribute('src', timeSrc);
 
 
     // remove the d-none class if present
